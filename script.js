fc1d27cda585bad10b01c39be7027d8ee61b21a3
Shivani Saha <67801968+shivani-prog@users.noreply.github.com>
2021-09-20 23:46:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+document.getElementById('button').addEventListener('click',
+function(){
+    document.querySelector('.info').style.display='flex';
+});
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+
+
+
+function validation(){
+  Swal.fire(
+  'Thank You!',
+  'Payment Successful!',
+  'success'
+)}
+
+document.getElementById(proceed).innerHTML("Pay"+Amount);
+  
